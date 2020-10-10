bb8b7911f373440d7f451d1109665b2dd3b6d7c5
Arvid Edenheim <arvidede@icloud.com>
2020-10-10 17:55:05 +0200

    Updates

---
+++ precache-manifest.eac4a016c7df171d72c1b37c5cd6ff3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7030f34961e9d5b3b22baa034420437c",
+    "url": "/ramen/index.html"
+  },
+  {
+    "revision": "c91171bb40e4968c093f",
+    "url": "/ramen/static/css/main.95365817.chunk.css"
+  },
+  {
+    "revision": "9d483722d9d8592afec2",
+    "url": "/ramen/static/js/2.45593619.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/ramen/static/js/2.45593619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91171bb40e4968c093f",
+    "url": "/ramen/static/js/main.33020437.chunk.js"
+  },
+  {
+    "revision": "00b806fd94f36e7663a1",
+    "url": "/ramen/static/js/runtime-main.2647f9f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90c07f1d6f8434f10876341fe9422134.js"
+  "/ramen/precache-manifest.eac4a016c7df171d72c1b37c5cd6ff3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ramen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
