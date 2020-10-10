a217a13b32a2e77179f3e58c8df2cce9f4cc13a2
Arvid Edenheim <arvidede@icloud.com>
2020-10-10 17:49:46 +0200

    Updates

---
+++ precache-manifest.90c07f1d6f8434f10876341fe9422134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9901d742c1fd1b76266711caeedda604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35caa03dfa407f0349d9",
+    "url": "/static/css/main.95365817.chunk.css"
+  },
+  {
+    "revision": "ebd48aaf9e5b740edc5c",
+    "url": "/static/js/2.d1ba785c.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.d1ba785c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35caa03dfa407f0349d9",
+    "url": "/static/js/main.9ce5bc64.chunk.js"
+  },
+  {
+    "revision": "6df8defb7fac4f8ad5c4",
+    "url": "/static/js/runtime-main.02737e24.js"
+  }
+]);
